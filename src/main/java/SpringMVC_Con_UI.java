51abc4c30756fc897b3895a726c90f932b44df88
Madhusmita <infosmita89@gmail.com>
2018-11-08 10:25:53 +0530

    done

--- src/main/java/SpringMVC_Con_UI.java
+++ src/main/java/SpringMVC_Con_UI.java
@@ -1,4 +1,14 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class SpringMVC_Con_UI {
+	@RequestMapping("/show")
+	public String show(ModelMap map) {
+		map.addAttribute("eid",12);
+		return "Home";
+		
+	}
 
 }
